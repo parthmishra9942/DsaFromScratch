04c26d624e608690323bd49e9702d6912ba836c1
ParthMishra <136218849+parthmishra9942@users.noreply.github.com>
2026-07-05 00:10:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.py
@@ -0,0 +1,18 @@
+class Solution:
+    def sort012(self, arr):
+        # code here
+        mid=0
+        low=0
+        high =len(arr)-1
+        
+        while mid<=high:
+            if arr[mid]==0:
+                arr[low],arr[mid]=arr[mid],arr[low]
+                mid+=1
+                low+=1
+            elif arr[mid]==1:
+                mid+=1
+            else:
+                arr[mid],arr[high]=arr[high],arr[mid]
+                high-=1
+        return arr
