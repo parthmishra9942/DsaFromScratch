febee40bd837fa160b3a98e25800a180549bd4c0
ParthMishra <136218849+parthmishra9942@users.noreply.github.com>
2026-07-06 13:02:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Two Sum - Pair with Given Sum/two-sum-pair-with-given-sum.py
+++ Difficulty: Easy/Two Sum - Pair with Given Sum/two-sum-pair-with-given-sum.py
@@ -1,19 +1,18 @@
 class Solution:
-	def twoSum(self, arr, target):
-	    arr.sort()
-		i=0
-		j=len(arr)-1
-		
-		while i<j:
-		    curr_sum = arr[i]+arr[j]
-		    
-		    if curr_sum == target:
-		        return [i,j]
-		    elif curr_sum<target:
-		        i+=1
-		    else:
-		        j-=1
-	    return []
-		           
-		
-		
+    def twoSum(self, arr, target):
+        arr.sort()
+
+        i = 0
+        j = len(arr) - 1
+
+        while i < j:
+            curr_sum = arr[i] + arr[j]
+
+            if curr_sum == target:
+                return True
+            elif curr_sum < target:
+                i += 1
+            else:
+                j -= 1
+
+        return False
